b792f4987f97fd9e2d448b5a1b05971eeaae0d7d
Juan Naputi <jnaputi253@gmail.com>
2017-09-01 01:37:18 -0700

    Moved items to their own directories

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+require('./services/passport');
 
 const app = express();
-passport.use(new GoogleStrategy());
+require('./routes/authRoutes')(app);
 
 app.listen(process.env.PORT || 5000);
